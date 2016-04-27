97757d1b1d862426adf5cccc96c92671d0e7230f
Manas Sharma <manas2.sharma@gmail.com>
2016-04-27 19:13:02 +0530

    microservice 1 - New file changes

---
+++ uservice1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("\n Hello World ... ");
+	int a = 10;
+	while (a != 0) {
+		printf("\n \t value is %d", a--);
+	}
+	printf("\n exiting\n");
+	return;
+}
