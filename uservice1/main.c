38d1de0e2406bd70ab2b94454a70e86fdd951546
Manas Sharma <manas2.sharma@gmail.com>
2016-05-10 12:21:29 +0530

    Minor changes to healthcheck

--- uservice1/main.c
+++ uservice1/main.c
@@ -7,19 +7,19 @@ int main()
 	int a = 10, exit_val = 0;
 	char cmd_str[128];
 
-	printf("\n Hello World ... ");
+	printf("\n Hello World ... \n");
 	printf("\n \t value is %d\n", a);
 
 	while (1) {
-		sprintf(cmd_str, "echo \"%d,\" >> /tmp/health", a++);
+		sprintf(cmd_str, "echo \"%d,\" >> /tmp/health \n", a++);
 		system (cmd_str);
 		sleep(1);
 		if (a == 30) {
 			system ("rm -rf /tmp/health");
-			printf("\n continuing...%d\n",a);
+			printf("\n exiting ...%d\n",a);
 			return;
 		}
-		printf("\n exiting ...%d\n",a);
+		printf("\n continuing ...%d\n",a);
 #if 0
 		scanf("%d\n", &exit_val);
 		if (exit_val == 100) {
