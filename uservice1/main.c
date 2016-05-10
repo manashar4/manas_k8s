6a992b081df1e6917450c3a9ba024d54d32432da
Manas Sharma <manas2.sharma@gmail.com>
2016-05-10 11:32:20 +0530

    additional enhancements to build health checks.

--- uservice1/main.c
+++ uservice1/main.c
@@ -5,14 +5,21 @@
 int main()
 {
 	int a = 10, exit_val = 0;
+	char cmd_str[128];
 
 	printf("\n Hello World ... ");
 	printf("\n \t value is %d\n", a);
 
 	while (1) {
-		system("echo 100 > /tmp/health");
-		sleep(5);
-		printf("\n exiting ...\n");
+		sprintf(cmd_str, "echo \"%d,\" >> /tmp/health", a++);
+		system (cmd_str);
+		sleep(1);
+		if (a == 30) {
+			system ("rm -rf /tmp/health");
+			printf("\n continuing...%d\n",a);
+			return;
+		}
+		printf("\n exiting ...%d\n",a);
 #if 0
 		scanf("%d\n", &exit_val);
 		if (exit_val == 100) {
