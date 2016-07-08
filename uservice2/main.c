5ed3a0e86fd0003bc50ce0cfaf089947e089b0f9
Manas <msharma@ubuntu1404-virtual-machine>
2016-07-08 22:42:43 +0530

    adding log4c logs

--- uservice2/main.c
+++ uservice2/main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "log4c.h"
 
 int main()
 {
 	int a = 10, exit_val = 0;
 	char cmd_str[128];
+	log4c_category_t* mycat = NULL;
 
-	printf("\n Hello World ... \n");
-	printf("\n \t value is %d\n", a);
+
+	log4c_init();
+    mycat = log4c_category_get("manas.usrv2");
+
+	log4c_category_log(mycat, LOG4C_PRIORITY_DEBUG, "Hello World ... \n");
+	log4c_category_log(mycat, LOG4C_PRIORITY_DEBUG, "value is %d\n", a);
 
 	while (1) {
 		sleep(5);
 		if (a == 10) {
-			printf("\n exiting ...%d\n",(a/0));
+	        log4c_category_log(mycat, LOG4C_PRIORITY_ERROR, "exiting ...%d\n",a);
 			return;
 		}
-		printf("\n continuing ...%d\n",a);
+		log4c_category_log(mycat, LOG4C_PRIORITY_WARNING, "continuing ...%d\n",a);
 		fflush(stdout);
         a++;
 #if 0
@@ -26,6 +32,9 @@ int main()
 		}
 #endif
 	}
+    if ( log4c_fini()){
+        printf("log4c_fini() failed");
+    }
 
 	return;
 }
